7a8eb600511abf84a089e50af1ac36510b2b2eaf
Chris Hegre <christopher.hegre@bigcommerce.com>
2015-03-10 19:05:50 -0700

    Mobile view start

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 module.exports = {
-    API_HOST: "http://192.168.33.10:3001",
+    API_HOST: "http://192.168.33.10:3001/v1",
     CDN_ROOT: "http://192.168.33.10", //vagrant dev setting, links to /opt/immodispo/
     api: {
         username: 'web',
--- shipitfile.js
+++ shipitfile.js
@@ -12,19 +12,19 @@ module.exports = function (shipit) {
             keepReleases: 3
         },
         staging: {
-            servers: 'webuser@caketoast.com'
+            servers: 'nodeapps@caketoast.com'
         },
         production: {
-            servers: 'webuser@mealtrap.com'
+            servers: 'nodeapps@mealtrap.com'
         }
     });
 
     shipit.on('published', function() {
         shipit.remote('cd /opt/immodispo-web/current && npm install').then(function(res) {
             shipit.log(res);
-            shipit.remote('cp /home/webuser/env.js /opt/immodispo-web/current').then(function(res) {
+            shipit.remote('cp /home/nodeapps/web-env.js /opt/immodispo-web/current/env.js').then(function(res) {
                 shipit.log(res);
-                shipit.remote('pm2 restart all').then(function(res) {
+                shipit.remote('pm2 reload web').then(function(res) {
                     shipit.log(res);
                 });
             });
